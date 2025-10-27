153d026c4474b79c2896d5d0ab70cf2b017944ff
HuygiaTon-2011 <giahuy7920112004@gmail.com>
2025-10-27 16:12:44 +0700

    Thêm toàn bộ source code website bán quần áo

---
+++ huygiaton2011/views/about/about.php
@@ -0,0 +1,46 @@
+<section id="halink_vn_homepage_content" style="margin-top:  18%;">
+ 	<div class="container">
+	<!-- Title -->
+        <div class="title-wrapper  woodmart-title-color-default woodmart-title-style-default woodmart-title-width-100 text-center woodmart-title-size-medium ">
+            <div class="liner-continer">
+            	<span class="left-line"></span>
+            	<h4 class="wle-upper woodmart-title-container title  woodmart-font-weight-">Giới Thiệu</h4>
+            	<span class="right-line"></span>
+            </div>
+        </div>
+	</div>
+</section>
+<!-- pages-title-end -->
+<!-- about-us-section-start -->
+<div class="pages about-us section-padding">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-6">
+					<div class="about-img">
+						<img src="public/images/baxa.jpg" alt=""  />
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="about-text">
+						<div class="row">
+						<h4>Cửa hàng thời trang thể thao Tùng Trần</h4>
+						<ol>
+						<li>Cửa hàng thời trang thể thao Tùng Trần là một cửa hàng thời trang chuyên cung cấp các loại sản phẩm về áo, quần, mũ, tất thể thao bóng đá.</li>
+						<li>Luôn cập nhật những sản phẩm mới nhất mỗi ngày, Tùng mong muốn đem lại cho khách hàng những giá trị tốt nhất về sản phẩm cũng như các dịch vụ tại cửa hàng của Tùng.</li>
+						<li>Với không gian mua sắm sang trọng, hiện đại, đội ngũ nhân viên tận tình, chu đáo, cùng với đó là dịch vụ chăm sóc khách hàng vô cùng hoàn hảo, Tùng hy vọng tất cả khách hàng sẽ có những giây phút shopping thật thoải mái.</li>
+						</ol>
+						</div>
+						<div class="row" style="margin-top: 5px">
+						<h4>Cửa hàng chúng tôi với:</h4>
+						<ol>
+							<li>Các chương trình khuyến mãi hấp dẫn</li>
+							<li>Thái độ phục vụ tốt</li>
+							<li>Nguồn cung cấp uy tín và ổn định</li>
+						</ol>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
+</div>
+<!-- about-us section end -->
---
+++ huygiaton2011/views/news/news.php
@@ -0,0 +1,268 @@
+<section id="halink_vn_homepage_content" style="margin-top:  18%;">
+ 	<div class="container">
+	<!-- Title -->
+        <div class="title-wrapper  woodmart-title-color-default woodmart-title-style-default woodmart-title-width-100 text-center woodmart-title-size-medium ">
+            <div class="liner-continer">
+            	<span class="left-line"></span>
+            	<h4 class="wle-upper woodmart-title-container title  woodmart-font-weight-">Tin Tức</h4>
+            	<span class="right-line"></span>
+            </div>
+        </div>
+	</div>
+</section>
+<div class="container" >
+	<div class="row" >
+		<div class="col-12">
+			<div class="owl-carousel owl-carousel-tintuc1 owl-theme">
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/mp3.png" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Mbappe từ bỏ 270 triệu USD để gia nhập Real</h5>
+						    <p class="card-text">
+						    	Đăng bởi : 
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Tây Ban NhaNếu ở lại PSG, Kylian Mbappe có thể kiếm được nhiều hơn Cristiano Ronaldo tại Al Nassr.
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/messi.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>24 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Scaloni: 'Đừng khiến Messi phát điên về World Cup 2026'</h5>
+						    <p class="card-text">
+						    	Đăng bởi :
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							HLV Lionel Scaloni kêu gọi truyền thông và người hâm mộ Argentina ngừng đặt vấn đề liệu Lionel Messi có tham dự World Cup 2026 hay không.
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/brazil.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">
+							Vì sao Brazil cần HLV nước ngoài chất lượng?</h5>
+						    <p class="card-text">
+						    	Đăng bởi :
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Trận thua thảm kình địch Argentina ở vòng loại World Cup 2026 đặt ra yêu cầu cấp bách về vị trí HLV cho Brazil....
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>	
+    		</div>
+    	</div>			
+    </div>
+    <br> <br>
+    <div class="row" >
+		<div class="col-12">
+			<div class="owl-carousel owl-carousel-tintuc1 owl-theme">
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/tuanhai.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29<br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Phạm Tuấn Hải thất vọng vì xuống phong độ sau ASEAN Cup</h5>
+						    <p class="card-text">
+						    	Đăng bởi : 
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Tiền đạo Phạm Tuấn Hải buồn vì không thể cải thiện được phong độ ở CLB lẫn đội tuyển Việt Nam sau chức vô địch ASEAN Cup 2024. ...
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/raphinha.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Raphinha đổ lỗi cho phiên dịch sau tuyên bố hạ nhục Argentina</h5>
+						    <p class="card-text">
+						    	Đăng bởi :
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Argentina Tiền đạo cánh của Brazil giải thích với đồng nghiệp Nicolas Tagliafico rằng tuyên bố của anh bị dịch sai từ tiếng Bồ Đào Nha. ..
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/indo.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Điều kiện để Indonesia vào thẳng VCK World Cup 2026</h5>
+						    <p class="card-text">
+						    	Đăng bởi :
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Indonesia Chiến thắng trước Bahrain giúp Indonesia nuôi hy vọng chiếm vị trí thứ hai của Australia ở bảng C để giành vé chính thức dự World Cup 2026....
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>	
+    		</div>
+    	</div>			
+    </div> <br> <br>
+    <div class="row" >
+		<div class="col-12">
+			<div class="owl-carousel owl-carousel-tintuc1 owl-theme">
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/achen.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Argentina vùi dập Brazil trong ngày giành vé dự World Cup</h5>
+						    <p class="card-text">
+						    	Đăng bởi : 
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text"> 
+							Argentina Tâm lý thoải mái sau khi giành vé World Cup sớm năm lượt trận giúp Argentina thăng hoa trên sân nhà khi thắng đậm kình địch Brazil 4-1 hôm 25/3....
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/yama.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Yamal tiếp tục đáp trả cựu danh thủ Hà Lan</h5>
+						    <p class="card-text">
+						    	Đăng bởi :
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Tây Ban Nha Tiền đạo 17 tuổi Lamine Yamal đăng thêm video lên mạng xã hội nhằm đáp trả Rafael Van der Vaart, cựu tiền vệ tuyển Hà Lan và Real Madrid. ...
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>
+	    		<div class="item item_cardtintuc">
+	    			<div class="card card_tintuc" >
+	  					<img src="public/images/vietnam.jpg" class="card-img-top img_tintuc" alt="...">
+	  					<div class="day_tintuc">
+	  						<span>29 <br> TH3</span>
+	  						
+	  					</div>
+  						<div class="card-body">
+						    <h5 class="card-title title_card">Việt Nam tăng 5 bậc trên bảng FIFA</h5>
+						    <p class="card-text">
+						    	Đăng bởi :
+						    	<span>
+						    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+									  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+									</svg>
+						    	</span>
+						    	Admin
+						    </p>
+						    <p class="card-text">
+							Chuỗi thắng 7 trận giúp Việt Nam dự kiến được cộng gần 20 điểm, để tăng 5 bậc lên thứ 109 trên bảng FIFA....
+						    </p>
+						    <a href="#" class="btn btn-primary">Chi Tiết</a>
+					  </div>
+					</div>
+	    		</div>	
+    		</div>
+    	</div>			
+    </div>
+</div>
